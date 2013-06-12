c40e33c135b44909a59835b25b763fb67020f0ca
Bart Spiers <bart.apella@gmail.com>
2013-06-12 23:24:22 +0200

    removed solarized-theme from the prelude-packages

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -46,8 +46,9 @@
     git-commit-mode gitconfig-mode gitignore-mode
     guru-mode helm helm-projectile ido-ubiquitous
     key-chord magit melpa rainbow-mode
-    smex solarized-theme undo-tree
-    volatile-highlights yasnippet zenburn-theme)
+    smex undo-tree
+    volatile-highlights
+    yasnippet zenburn-theme)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
