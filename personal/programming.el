dda8356fced44d96505a5a485326ddfe74c646a0
localadmin <localadmin@ubuntu.(none)>
2013-06-24 18:12:55 +0200

    some extra c++ file types and a prettier text format

--- personal/programming.el
+++ personal/programming.el
@@ -1,3 +1,5 @@
 
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.hpp$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.ocl$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cu$" . c++-mode))
