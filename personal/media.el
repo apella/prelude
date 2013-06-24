47b4bd1a1fc7997f7e26285542e949348f59ed3d
localadmin <localadmin@ubuntu.(none)>
2013-06-24 18:15:21 +0200

    Added a default directory (doesn't seem to do much)
    Added useage of emms-volume.
    Use + and - inside an emms buffer to control the volume.

--- personal/media.el
+++ personal/media.el
@@ -6,7 +6,11 @@
 (emms-all)
 (emms-default-players)
 (global-set-key (kbd "<f2>") 'emms-smart-browse)
+(setq emms-source-file-default-directory "~/Dropbox/Music/")
 
 ;; when you have compiled emms-print-metadata we can use that to gather the metadata for in the library
 (require 'emms-info-libtag)
 (setq emms-info-functions '(emms-info-libtag))
+
+;; use the volume controls of ems
+(require 'emms-volume)
