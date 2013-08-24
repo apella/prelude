07ad894e480ac61145d508b87ed8f073fe4e937f
Bart Spiers <bart.apella@gmail.com>
2013-08-24 19:25:48 +0200

    commented out the media modules
    The settings are system-dependent, I'll add emms as a sub-repository.

--- personal/media.el
+++ personal/media.el
@@ -1,17 +1,17 @@
 ;;; loads the emms module
 
 
-(add-to-list 'load-path "~/emms/lisp")
-(require 'emms-setup)
-(emms-all)
-(emms-default-players)
-(global-set-key (kbd "<f2>") 'emms-smart-browse)
-(setq emms-source-file-default-directory "~/Dropbox/Music/")
+;; (add-to-list 'load-path "~/emms/lisp")
+;; (require 'emms-setup)
+;; (emms-all)
+;; (emms-default-players)
+;; (global-set-key (kbd "<f2>") 'emms-smart-browse)
+;; (setq emms-source-file-default-directory "~/Dropbox/Music/")
 
-;; when you have compiled emms-print-metadata we can use that to gather the
-;; metadata for in the library
-(require 'emms-info-libtag)
-(setq emms-info-functions '(emms-info-libtag))
+;; ;; when you have compiled emms-print-metadata we can use that to gather the
+;; ;; metadata for in the library
+;; (require 'emms-info-libtag)
+;; (setq emms-info-functions '(emms-info-libtag))
 
-;; use the volume controls of ems
-(require 'emms-volume)
+;; ;; use the volume controls of ems
+;; (require 'emms-volume)
