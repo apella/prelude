b47de03316dc25f9788185b26cad6aff8473874f
Bart Spiers <bart.spiers@vub.ac.be>
2013-09-02 18:02:06 +0200

    added toggle-frame-split

--- personal/global-settings.el
+++ personal/global-settings.el
@@ -85,7 +85,19 @@
 (global-set-key (kbd "C-c r") 'rename-file-and-buffer)
 
 
+(defun toggle-frame-split ()
+  "If the frame is split vertically, split it horizontally or vice versa.
+Assumes that the frame is only split into two."
+  (interactive)
+  (unless (= (length (window-list)) 2) (error "Can only toggle a frame split in two"))
+  (let ((split-vertically-p (window-combined-p)))
+    (delete-window) ; closes current window
+    (if split-vertically-p
+        (split-window-horizontally)
+      (split-window-vertically)) ; gives us a split with the other window twice
+    (switch-to-buffer nil))) ; restore the original window in this part of the frame
 
+(global-set-key (kbd "C-x t") 'toggle-frame-split)
 
 ;; default with window-numbers on
 ;; ----------------------------------------------------------------------------
